878bebad3fb9e3dd6d1664ed59a8f5544898f3ce
Kshitiz Upreti <upreti.kshitiz.ku@gmail.com>
2022-06-26 20:47:33 +0545

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,31 @@
 import React, { useState } from "react";
-// import Todolist from "./todo";
 
 const App = () => {
-  const [value,setvalue]=useState(0);
-  const i=()=>{
-    setvalue(value+1);
-  }
-  const d=()=>{
-    if (value<=0){
-      {alert("LIMIT TO 0")}
+  const [value, setvalue] = useState(0);
+  const i = () => {
+    setvalue(value + 1);
+  };
+  const d = () => {
+    if (value <= 0) {
+      {
+        alert("LIMIT TO 0");
+      }
       setvalue(value);
+    } else {
+      setvalue(value - 1);
     }
-    else{
-      setvalue(value-1);
-    }
-    
-  }
+  };
   return (
     <>
       <div className="main_div">
         <div className="center_div">
           <h1>{value}</h1>
-          <button id="d" onClick={d}>Decre</button>
-          <button id="i" onClick={i}>Incre</button>
+          <button id="d" onClick={d}>
+            Decre
+          </button>
+          <button id="i" onClick={i}>
+            Incre
+          </button>
         </div>
       </div>
     </>
